06f35ca813c86ba7eee20ea5110066334aab0b93
jjpcyrl <joaopedrodossantosbispo310@gmail.com>
2026-07-10 09:07:41 -0300

    feature apainel adimin

--- backend/routes/agendamentoRoutes.js
+++ backend/routes/agendamentoRoutes.js
@@ -10,5 +10,7 @@ router.use(authMiddleware);
 router.post('/', agendamentoController.criar);
 router.put('/:id/cancelar', agendamentoController.cancelar);
 router.get('/meus', agendamentoController.listarMeus);
+// Adicione esta linha junto das outras rotas (pode ser logo abaixo do router.get('/meus', ...))
+router.get('/admin/todos', agendamentoController.listarTodos);
 
 module.exports = router;
